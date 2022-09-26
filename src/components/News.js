218653188b4d66c4b78d8707f193c7c6da147c02
Abhishek Soni <abhishek1600soni@gmail.com>
2022-09-26 14:57:03 +0530

    Handled API error situations

--- src/components/News.js
+++ src/components/News.js
@@ -5,7 +5,9 @@ import Loader from './Loader';
 const News = (props) => {
     const [articles, setArticles] = useState([]);
     const [loading, setLoading] = useState(true);
-const apiCall = async () => {
+    const [apiError, setApiError] = useState(false);
+
+    const apiCall = async () => {
         const apiOptions = {
             method: 'GET',
             headers: {
@@ -13,42 +15,50 @@ const apiCall = async () => {
                 'X-RapidAPI-Key': `${props.apiKey}`
             }
         };
-        setLoading(true);  // initially set loading="true"
-        fetch("https://current-news.p.rapidapi.com/news", apiOptions)
-            .then(response => response.json()) //set loading="true" while fetching data
-            // this is an ARROW FUNCTION -> same as .then((response) =>{response.json()}}
-            .then(result => {       // the value of "response" is stored in "result"
-                // console.log(result);
+        setLoading(true);  // initially set loading="true" while fetching data
+        await fetch("https://current-news.p.rapidapi.com/news", apiOptions)
+            .then(result => result.json())
+            .then(result => {
                 setArticles(result.news); // "news" is property of "result" object, which contains main object values
                 setLoading(false);
+                if (!result.ok) {
+                    setApiError(true);
+                }
             })
-            // catch will be executed if the fetch promise fails(means when there is error in fatching api url)
-            .catch(err => console.error(err));
+            .catch(err => {
+                console.error(err);
+                // If api is configured correctly, but having some error in request, set apiError to true
+                setApiError(true);
+            });
     }
-    
+
     useEffect(() => {
         apiCall();
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    },[])
+    }, [])
 
     return (
         <div className='container my-3'>
-            <h2 className="text-center" style={{marginTop:"68px"}}>Latest News</h2>
+            <h2 className="text-center" style={{ marginTop: "68px" }}>Latest News</h2>
             <div style={{ position: "relative", top: "30vh" }}>
-                {loading && <Loader />}             {/* show loader if loading state is true */}
+                {loading && !apiError && <Loader />}
             </div>
-            <div className='row'>
-                {!loading && articles.map((element) => {   // here, elements represents "articles" object
-                    // map function will render(show) <div> tag times equal to number of objects inside "articles" object
-
-                    return <div key={element.url} className='col-md-4 col-sm-12'>
-                        <NewsItem title={element.title ? element.title?.slice(0, 68) : "No Title Available"}
-                            description={element.description ? element.description?.slice(0, 90) : "No Description available for this news. Please click on Read More buttion to read full news"}  //slice method will set how many characters we want to display
-                            imgUrl={element.urlToImage} newsUrl={element.url} />
+            {
+                apiError ? <div className="apiError">
+                    <b> Error in Fetching News Articles !<br /> Please Try Again After Sometime.</b>
+                </div>
+                    :
+                    <div className='row'>
+                        {(!loading && articles !== undefined) && articles.map((element) => {   // here, elements represents "articles" object
+                            // map function will render(show) <div> tag times equal to number of objects inside "articles" object
+                            return <div key={element.url} className='col-md-4 col-sm-12'>
+                                <NewsItem title={element.title ? element.title?.slice(0, 68) : "No Title Available"}
+                                    description={element.description ? element.description?.slice(0, 90) : "No Description available for this news. Please click on Read More buttion to read full news"}  //slice method will set how many characters we want to display
+                                    imgUrl={element.urlToImage} newsUrl={element.url} />
+                            </div>
+                        })}
                     </div>
-                })}
-
-            </div>
+            }
         </div>
     )
 }
