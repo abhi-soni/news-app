973537bc6362ee2a219bd4a13c59e561d077f24d
Abhishek Soni <abhishek1600soni@gmail.com>
2023-05-12 19:39:59 +0530

    Fixed API Error Bugs

--- src/components/News.js
+++ src/components/News.js
@@ -5,49 +5,58 @@ import Loader from './Loader';
 const News = (props) => {
     const [articles, setArticles] = useState([]);
     const [loading, setLoading] = useState(true);
-    const [apiError, setApiError] = useState(false);
-
+    const [error, setError] = useState('');
+    const apiOptions = {
+        method: 'GET',
+    };
+    
     useEffect(() => {
         // eslint-disable-next-line react-hooks/exhaustive-deps
-        const apiOptions = {
-            method: 'GET',
-        };
         setLoading(true);  // initially set loading="true" while fetching data
         fetch(`https://gnews.io/api/v4/top-headlines?token=${props.apiKey}&lang=en`, apiOptions)
             .then(result => result.json())
             .then(result => {
                 setArticles(result.articles); // "articles" is property of "result" object, which contains main object values
                 setLoading(false);
+                setError('');
             })
             .catch(err => {
                 console.error(err);
                 // If api is configured correctly, but having some error in request, set apiError to true
-                setApiError(true);
-                console.log(apiError)
+                setLoading(false);
+                setError('Error fetching data. Please try again later.');
             });
-    }, [apiError, props.apiKey])
-
-    return (
-        <div className='container my-3'>
+        }, [props.apiKey])
+        
+        return (
+            <div className='container my-3'>
             <h2 className="text-center" style={{ marginTop: "68px" }}>Latest News</h2>
             <div style={{ position: "relative", top: "30vh" }}>
-                {loading && !apiError && <Loader />}
+                {loading && <Loader />}
             </div>
-            {
-                apiError ? ''
-                    :
+            {error ? (
+                <p style={errorCss}>{error}</p>
+                ) : (
                     <div className='row'>
-                        {(!loading && articles !== undefined) && articles.map((element) => {   // here, elements represents "articles" object
-                            // map function will render(show) <div> tag times equal to number of objects inside "articles" object
-                            return <div key={element.url} className='col-md-4 col-sm-12'>
-                                <NewsItem title={element.title ? element.title?.slice(0, 68) : "No Title Available"}
-                                    description={element.description ? element.description?.slice(0, 90) : "No Description available for this news. Please click on Read More buttion to read full news"}  //slice method will set how many characters we want to display
-                                    imgUrl={element.image} newsUrl={element.url} />
-                            </div>
-                        })}
-                    </div>
-            }
+                    {(!loading && articles !== undefined) && articles.map((element) => {   // here, elements represents "articles" object
+                        // map function will render(show) <div> tag times equal to number of objects inside "articles" object
+                        return <div key={element.url} className='col-md-4 col-sm-12'>
+                            <NewsItem title={element.title ? element.title?.slice(0, 68) : "No Title Available"}
+                                description={element.description ? element.description?.slice(0, 90) : "No Description available for this news. Please click on Read More buttion to read full news"}  //slice method will set how many characters we want to display
+                                imgUrl={element.image} newsUrl={element.url} />
+                        </div>
+                    })}
+                </div>
+            )}
         </div>
     )
 }
 export default News;
+
+const errorCss = {
+    color: "red",
+    textAlign: "center",
+    marginTop: "35vh",
+    fontSize: "30px",
+    fontWeight: "bold",
+}
