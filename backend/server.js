d5e288ce0cbce1687fa1c2f93f4c637eabbf85cb
Abhishek Soni <abhishek1600soni@gmail.com>
2023-05-27 14:48:42 +0530

    Cors config changed

--- backend/server.js
+++ backend/server.js
@@ -7,33 +7,29 @@ const PORT = process.env.PORT || 4000;
 const api = `https://gnews.io/api/v4/top-headlines?token=${process.env.REACT_APP_API_KEY}&lang=en`;
 
 const allowedOrigin = 'https://news-app-abhishek.netlify.app';
-const corsOptions = {
-    origin: function (origin, callback) {
-        if (origin === allowedOrigin) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
+const checkReferer = (req, res, next) => {
+    const referer = req.headers.referer;
+    if (!referer || !referer.includes(allowedOrigin)) {
+        res.status(403).send('Access Forbidden');
+        return;
+    }
+    next();
 };
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.redirect('/api');
 });
-app.get('/api', (req, res) => {
-    if (req.headers.referer && req.headers.referer.includes(allowedOrigin)) {
-        axios.get(api)
-            .then(response => {
-                res.send(response.data);
-            })
-            .catch(err => {
-                console.error(err);
-            });
-    } else {
-        res.status(403).send('403: Access Forbidden');
-    }
+app.get('/api', checkReferer, (req, res) => {
+
+    axios.get(api)
+        .then(response => {
+            res.send(response.data);
+        })
+        .catch(err => {
+            console.error(err);
+        });
 });
 
 app.listen(PORT, () => {
